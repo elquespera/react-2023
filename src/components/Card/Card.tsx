fd19e9037d1eed4a81fa9face08effe4211cc143
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-03-17 16:17:56 +0100

    feat: card styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -15,16 +15,20 @@ export default class Card extends React.Component<CardProps> {
     const { description, address, price, dateAdded } = this.props.data;
     return (
       <li className={styles.card}>
-        <img
-          className={styles.image}
-          src={`${UNSPLASH_URL}&sig=${Math.floor(Math.random() * 200)}`}
-          alt={description}
-        />
+        <div className={styles.imageWrapper}>
+          <img
+            className={styles.image}
+            src={`${UNSPLASH_URL}&sig=${Math.floor(Math.random() * 200)}`}
+            alt={description}
+          />
+        </div>
         <span className={styles.price}>${price}</span>
         <div className={styles.body}>
           <h3 className={styles.description}>{description}</h3>
           <span className={styles.address}>{address}</span>
-          <span className={styles.date}>{new Date(dateAdded).toLocaleDateString()}</span>
+          <span className={styles.date}>{`Added on ${new Date(dateAdded).toLocaleDateString(
+            'en'
+          )}`}</span>
         </div>
       </li>
     );
