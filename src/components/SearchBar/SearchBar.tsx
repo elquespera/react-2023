965a389a8651c47ff3fbf91edcee4ac6fa346855
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-03-17 12:30:30 +0100

    feat: add search bar component & install sass

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './SearchBar.module.scss';
+
+export default class SearchBar extends React.Component {
+  render() {
+    return <div className={styles.wrapper}>SearchBar</div>;
+  }
+}
--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import SearchBar from '../components/SearchBar/SearchBar';
 
 export default class Main extends React.Component {
   render() {
-    return <div>Main</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
   }
 }
