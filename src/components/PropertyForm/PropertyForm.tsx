03d4f0ed6e623401899c5051f3d58cd7ddc26ce3
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-03-22 19:37:48 +0100

    feat: add error labels

--- src/components/PropertyForm/PropertyForm.tsx
+++ src/components/PropertyForm/PropertyForm.tsx
@@ -5,51 +5,108 @@ interface PropertyFormProps {
   onSubmit?: () => void;
 }
 
-export default class PropertyForm extends React.Component<PropertyFormProps> {
+interface ValidationErrors {
+  title?: boolean;
+  address?: boolean;
+  price?: boolean;
+  rooms?: boolean;
+  availableFrom?: boolean;
+  sellOrRent?: boolean;
+  agreeToTerms?: boolean;
+}
+
+interface PropertyFormState {
+  errors: ValidationErrors;
+}
+
+export default class PropertyForm extends React.Component<PropertyFormProps, PropertyFormState> {
   constructor(props: PropertyFormProps) {
     super(props);
+    this.state = { errors: {} };
   }
+
+  setErrors(newErrors: ValidationErrors) {
+    this.setState((current) => {
+      return { errors: { ...newErrors, ...current.errors } };
+    });
+  }
+
   render() {
+    const errors = this.state?.errors;
     return (
       <form className={styles.form}>
         <h3>Add new property</h3>
         <div className={styles.table}>
           <label htmlFor="property-title">Title</label>
-          <input type="text" id="property-title" />
+          <div className={styles.inputWrapper}>
+            <input type="text" id="property-title" />
+            {errors?.title && <span className={styles.error}>Please provide property title</span>}
+          </div>
 
           <label htmlFor="property-address">Address</label>
-          <input type="text" id="property-address" />
+          <div className={styles.inputWrapper}>
+            <input type="text" id="property-address" />
+            {errors?.address && (
+              <span className={styles.error}>Please provide property address</span>
+            )}
+          </div>
 
-          <label htmlFor="property-price">Address</label>
-          <input type="text" id="property-price" />
+          <label htmlFor="property-price">Price</label>
+          <div className={styles.inputWrapper}>
+            <input type="number" id="property-price" />
+            {errors?.price && <span className={styles.error}>Please provide property price</span>}
+          </div>
 
           <label htmlFor="property-rooms">Rooms</label>
-          <select id="property-rooms">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5+</option>
-          </select>
+          <div className={styles.inputWrapper}>
+            <select id="property-rooms">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5+</option>
+            </select>
+            {errors?.rooms && (
+              <span className={styles.error}>Please select the number of rooms</span>
+            )}
+          </div>
 
           <label htmlFor="property-available-from">Available from</label>
-          <input type="date" id="property-available-from" />
+          <div className={styles.inputWrapper}>
+            <input type="date" id="property-available-from" />
+            {errors?.availableFrom && (
+              <span className={styles.error}>Please provide property availability</span>
+            )}
+          </div>
 
-          <label>Sell or rent</label>
-          <div>
-            <label className={styles.radioLabel}>
-              <input type="radio" name="sell-or-rent" value="sell" />
-              Sell
-            </label>
-            <label className={styles.radioLabel}>
-              <input type="radio" name="sell-or-rent" value="rent" />
-              Rent
-            </label>
+          <label>Purpose</label>
+          <div className={styles.inputWrapper}>
+            <div className={styles.radioWrapper}>
+              <label className={styles.radioLabel}>
+                <input type="radio" name="sell-or-rent" value="sell" />
+                Sell
+              </label>
+              <label className={styles.radioLabel}>
+                <input type="radio" name="sell-or-rent" value="rent" />
+                Rent
+              </label>
+            </div>
+            {errors?.sellOrRent && (
+              <span className={styles.error}>Please select the purpose of listing </span>
+            )}
           </div>
         </div>
-        <label className={styles.radioLabel}>
-          <input type="checkbox" />I want to receive email notifications about this property
-        </label>
+
+        <div className={styles.inputWrapper}>
+          <label className={styles.radioLabel}>
+            <input type="checkbox" />I agree to the Terms & Conditions
+          </label>
+          {errors?.agreeToTerms && (
+            <span className={styles.error}>
+              You have to agree to T&C before adding the property
+            </span>
+          )}
+        </div>
         <input type="submit" value="Add property" />
       </form>
     );
