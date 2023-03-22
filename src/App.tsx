944e64fc20c36daae7966e6079a5f1f4871b83ba
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-03-22 17:47:52 +0100

    feat: add route for adding property

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AboutUs from './routes/AboutUs';
+import AddProperty from './routes/AddProperty';
 import Layout from './routes/Layout';
 import Main from './routes/Main';
 import Route404 from './routes/Route404';
@@ -12,6 +13,10 @@ export default class App extends React.Component {
         <Routes>
           <Route path="/" element={<Layout title="Home" page={<Main />} />} />
           <Route path="/about-us" element={<Layout title="About Us" page={<AboutUs />} />} />
+          <Route
+            path="/add-property"
+            element={<Layout title="Add Property" page={<AddProperty />} />}
+          />
           <Route path="*" element={<Layout title="Not Found" page={<Route404 />} />} />
         </Routes>
       </BrowserRouter>
---
+++ src/routes/AddProperty.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class AddProperty extends React.Component {
+  render() {
+    return (
+      <form>
+        <h2>Add property</h2>
+      </form>
+    );
+  }
+}
