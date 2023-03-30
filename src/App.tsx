4da1af1e2b1c472763eeedf05fd1334fcba1e92c
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-03-30 21:19:50 +0200

    refactor: app into function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ROUTES } from './routes';
 import Layout from './routes/Layout';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          {ROUTES.map(({ to, title, page: Page }) => (
-            <Route key={to} path={to} element={<Layout title={title} page={<Page />} />} />
-          ))}
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {ROUTES.map(({ to, title, page: Page }) => (
+          <Route key={to} path={to} element={<Layout title={title} page={<Page />} />} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
 }
