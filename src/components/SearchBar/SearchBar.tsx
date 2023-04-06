824febf9e929396b984c5c9c56cfe2a795d4f2a9
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-04-06 05:41:27 +0200

    feat: add search submit form & styles

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,23 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { getLocalStorage, setLocalStorage } from '../../lib/storage';
 import Icon from '../Icon/Icon';
 import styles from './SearchBar.module.scss';
 
-export default function SearchBar() {
+interface SearchBarProps {
+  onSubmit?: (value: string) => void;
+}
+
+export default function SearchBar({ onSubmit }: SearchBarProps) {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const input = inputRef.current;
+
+    if (onSubmit && input) {
+      onSubmit(input.value);
+    }
+  };
 
   useEffect(() => {
     const { search } = getLocalStorage();
@@ -16,9 +29,16 @@ export default function SearchBar() {
   }, [inputRef]);
 
   return (
-    <label className={styles.wrapper}>
-      <Icon type="search" className={styles.icon} />
-      <input type="text" ref={inputRef} placeholder="Search" className={styles.input} />
-    </label>
+    <form onSubmit={handleSubmit}>
+      <div className={styles.wrapper}>
+        <label className={styles.inputWrapper}>
+          <Icon type="search" className={styles.icon} />
+          <input type="text" ref={inputRef} placeholder="Search" className={styles.input} />
+        </label>
+        <button type="submit" className={styles.submit} title="Search">
+          <Icon type="search" />
+        </button>
+      </div>
+    </form>
   );
 }
