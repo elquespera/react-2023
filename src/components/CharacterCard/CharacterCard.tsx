09a249c02651d085baf7ddfe90dc4021c997e8c4
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-04-06 13:17:28 +0200

    feat: add more info to modal

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Character } from '../../types';
 import Modal from '../Modal/Modal';
 import styles from './CharacterCard.module.scss';
+import clsx from 'clsx';
+import convertDate from '../../lib/convertDate';
 
 interface CharacterCardProps {
   data: Character;
@@ -10,7 +12,7 @@ interface CharacterCardProps {
 export default function CharacterCard({ data }: CharacterCardProps) {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const { name, image } = data;
+  const { name, image, species, status, created, location } = data;
   return (
     <>
       <li className={styles.card} onClick={() => setModalOpen(true)}>
@@ -22,6 +24,20 @@ export default function CharacterCard({ data }: CharacterCardProps) {
       <Modal open={modalOpen} title={name} onClose={() => setModalOpen(false)}>
         <div className={styles.modal}>
           <img className={styles.image} src={image} alt={name} />
+          <div className={styles.info}>
+            <span className={styles.species}>{species}</span>
+            <span
+              className={clsx(
+                styles.status,
+                status === 'Alive' && styles.alive,
+                status === 'Dead' && styles.dead
+              )}
+            >
+              {status}
+            </span>
+            <span className={styles.location}>Location: {location?.name}</span>
+            <span className={styles.created}>Created: {convertDate(created)}</span>
+          </div>
         </div>
       </Modal>
     </>
