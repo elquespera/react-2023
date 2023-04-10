048c4a253758942aaec7f9cdea974f59893fa130
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-04-10 13:37:34 +0200

    fix: add notifications when no characters are found

--- src/lib/fetchCharacters.ts
+++ src/lib/fetchCharacters.ts
@@ -1,17 +1,25 @@
 import { AllCharacters, Character } from '../types';
 import fetch from 'cross-fetch';
 
-export async function fetchAllCharacters(query?: string) {
+export async function fetchAllCharacters(query?: string): Promise<AllCharacters | undefined> {
   let url = 'https://rickandmortyapi.com/api/character';
   if (query) url += `?name=${query}`;
   const response = await fetch(url);
-  const data: AllCharacters = await response.json();
-  return data;
+  if (response.ok) {
+    const data: AllCharacters = await response.json();
+    return data;
+  } else {
+    throw new Error();
+  }
 }
 
-export async function fetchCharacter(id: number) {
+export async function fetchCharacter(id: number): Promise<Character | undefined> {
   const url = `https://rickandmortyapi.com/api/character/${id}`;
   const response = await fetch(url);
-  const data: Character = await response.json();
-  return data;
+  if (response.ok) {
+    const data: Character = await response.json();
+    return data;
+  } else {
+    throw new Error();
+  }
 }
--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -5,10 +5,12 @@ import CharacterCards from '../components/CharacterCards/CharacterCards';
 import Loader from '../components/Loader/Loader';
 import { fetchAllCharacters } from '../lib/fetchCharacters';
 import { getLocalStorage } from '../lib/storage';
+import SnackBar from '../components/SnackBar/SnackBar';
 
 export default function Main() {
   const [characters, setCharacters] = useState<Character[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState<string>();
 
   const handleSubmit = (value: string) => {
     findCharacters(value);
@@ -19,6 +21,9 @@ export default function Main() {
       setIsLoading(true);
       const result = await fetchAllCharacters(query);
       setCharacters(result?.results || []);
+      setErrorMsg(undefined);
+    } catch {
+      setErrorMsg('No characters were found');
     } finally {
       setIsLoading(false);
     }
@@ -30,10 +35,16 @@ export default function Main() {
   }, []);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '2rem' }} data-testid="home-page">
-      <SearchBar onSubmit={handleSubmit} />
-      <Loader visible={isLoading} />
-      <CharacterCards data={characters} />
-    </div>
+    <>
+      <div
+        style={{ display: 'flex', flexDirection: 'column', gap: '2rem' }}
+        data-testid="home-page"
+      >
+        <SearchBar onSubmit={handleSubmit} />
+        <Loader visible={isLoading} />
+        <CharacterCards data={characters} />
+      </div>
+      <SnackBar error title={errorMsg} onClose={() => setErrorMsg(undefined)} />
+    </>
   );
 }
