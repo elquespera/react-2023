c9424878ab714333acd0f6dfd787d536ca1915bc
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-04-10 13:49:18 +0200

    fix: fetch test

--- src/lib/fetchCharacters.test.ts
+++ src/lib/fetchCharacters.test.ts
@@ -4,6 +4,6 @@ import { fetchAllCharacters } from './fetchCharacters';
 describe('Fetch characters', () => {
   test('Shoud fetch all characters', async () => {
     const characters = await fetchAllCharacters();
-    expect(characters.info.count).toBeGreaterThan(0);
+    expect(characters?.info.count).toBeGreaterThan(0);
   });
 });
