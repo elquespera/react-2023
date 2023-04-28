ac35c1b0c96ba600ac4558024e7a029ee1e29a0f
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-04-28 17:49:29 +0200

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,24 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  test: {
-    setupFiles: './src/vitest.setup.ts',
-    globals: true,
-    environment: 'jsdom',
-    include: [
-      './src/components/**/*.test.tsx',
-      './src/routes/**/*.test.tsx',
-      './src/lib/**/*.test.tsx',
-    ],
-    coverage: {
-      reportsDirectory: '.coverage',
-      provider: 'c8',
-      reporter: ['text', 'json', 'html'],
-      all: true,
+export default defineConfig(() => {
+  return {
+    plugins: [react(), svgr()],
+    test: {
+      setupFiles: './src/vitest.setup.ts',
+      globals: true,
+      environment: 'jsdom',
+      include: [
+        './src/components/**/*.test.tsx',
+        './src/routes/**/*.test.tsx',
+        './src/lib/**/*.test.tsx',
+      ],
+      coverage: {
+        reportsDirectory: '.coverage',
+        provider: 'c8',
+        reporter: ['text', 'json', 'html'],
+        all: true,
+      },
     },
-  },
+  };
 });
